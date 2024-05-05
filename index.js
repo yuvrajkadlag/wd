e6c21b7532e4b26b9489d3ec6aa151fa0290dc4f
Yuvraj kadlag <166500306+yuvrajkadlag@users.noreply.github.com>
2024-05-05 12:04:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//npm init
+//download npm i mongodb 
+//npm i express
+//localhost:3000/deleteData/sai-delete
+//localhost:3000/insertData - post
+//localhost:3000/updateData/sai- put
+//localhost:3000/getData/sai -get
+const dbConnect=require('./mongodb')
+const express=require('express');
+const { response } = require('express');
+const app=express(); app.use(express.json())
+
+//get API
+app.get('/getData',async(req,res)=>{
+
+let result=await dbConnect();
+result=await result.find().toArray();
+res.send(result);
+})
+//post API
+app.post('/insertData',async(req,res)=>{
+let result=await dbConnect();
+result=await result.insertOne(req.body);
+res.send("Data Inserted Successfully")
+})
+
+// Put API
+app.put('/updateData/:name',async(req,res)=>{
+let result=await dbConnect();
+result=await result.updateOne({name:req.params.name},{$set:req.body});
+res.send("Data Updated Successfully")
+})
+
+//Delete API
+app.delete('/deleteData/:name',async(req,res)=>{
+let result=await dbConnect();
+result=await result.deleteOne({name:req.params.name})
+res.send("Data Deleted Successfully");
+})
+app.listen(3000);
